6859014c964a46257eadb27b6dfd60228ec96e17
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-29 14:15:55 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("---------------------------------------------");
-      System.out.println("        Bonjour Thibault, comment vas tu?    ");
+      System.out.println("   Bonjour Pierre Stéphane, comment vas tu?  ");
       System.out.println("---------------------------------------------");
                       
    }
